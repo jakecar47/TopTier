e25eccd0bfb217d817f6d139794813f2084986a3
jakecar47 <jakecar472@gmail.com>
2025-05-23 17:08:05 -0400

    Updated comment

--- toptier/src/lib/verifyToken.ts
+++ toptier/src/lib/verifyToken.ts
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
 
+// Verify a JWT 
 export function verifyToken(token: string) {
   try {
     return jwt.verify(token, JWT_SECRET);
